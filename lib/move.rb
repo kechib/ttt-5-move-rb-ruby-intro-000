d2bf8bf33c0fc4eb36b08cc3c1c3a7b4074eb1eb
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-24 20:36:38 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 input.to_i - 1
 end
 
-def move(board, index = O, value = "X")
+def move(array, index = O, value = "X")
 end
 
 def update_array_at_with(array, index, value)
