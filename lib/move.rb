4c310ec6108974e663fc6c547a9f998a31b96712
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-24 18:46:19 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-input_to_index "1" - "9"
-
 def update_array_at_with(array, index, value)
   array[index] = value
 end
